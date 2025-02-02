e6f3f71aefb387e0b1a0a8cc768c5bf01262367d
GonzaloHCZ <gohecaze@gmail.com>
2025-02-01 20:55:08 -0500

    Fondo Rediseñado

--- src/components/BackgroundStars.jsx
+++ src/components/BackgroundStars.jsx
@@ -34,10 +34,10 @@ const Stars = ({ title }) => {
         function crearEstrella(color) {
             const shape = new THREE.Shape();
             const radioExterior = 2;
-            const radioInterior = 1.1;
+            const radioInterior = 1.2;
             const numPuntas = 5;
             const anguloPaso = Math.PI / numPuntas;
-            const suavizado = 0.3; // Controla qué tan redondeadas son las puntas 0.4
+            const suavizado = 0.2; // Controla qué tan redondeadas son las puntas 0.4
 
             for (let i = 0; i <= numPuntas * 2; i++) {
                 const angulo = i * anguloPaso;
@@ -73,19 +73,45 @@ const Stars = ({ title }) => {
 
             const material = new THREE.MeshStandardMaterial({ 
                 color: color, 
-                metalness: 0.2, 
-                roughness: 0.3 
+                metalness: 0, 
+                roughness: 0 
             });
 
             return new THREE.Mesh(geometry, material);
         }
 
         // Función para crear una línea vertical
-        function crearLineaVertical() {
-            const geometry = new THREE.CylinderGeometry(0.08, 0.08, 10, 32);
-            const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+        function crearLineaVertical(altura) {
+            const geometry = new THREE.CylinderGeometry(0.05, 0.05, altura, 32);
+            const material = new THREE.ShaderMaterial({
+                uniforms: {
+                    color: { value: new THREE.Color(0xffffff) },
+                    opacityStart: { value: 1.0 },  // Opacidad en la base
+                    opacityEnd: { value: 0.0 }     // Opacidad en el otro extremo
+                },
+                vertexShader: `
+                    varying float vPositionY;
+                    void main() {
+                        vPositionY = position.y;
+                        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+                    }
+                `,
+                fragmentShader: `
+                    uniform vec3 color;
+                    uniform float opacityStart;
+                    uniform float opacityEnd;
+                    varying float vPositionY;
+
+                    void main() {
+                        float opacity = mix(opacityStart, opacityEnd, (vPositionY + 5.0) / 10.0); 
+                        gl_FragColor = vec4(color, opacity);
+                    }
+                `,
+                transparent: true
+            });
+            
+            
             const linea = new THREE.Mesh(geometry, material);
-            linea.rotation.x = Math.PI;
             return linea;
         }
 
@@ -98,7 +124,7 @@ const Stars = ({ title }) => {
         }
 
         // Función para crear una cruz redonda
-        function crearCruzRedonda(color, altura = 4, ancho = 2, grosor = 0.2) {
+        function crearCruzRedonda(color, altura = 4, ancho = 2, grosor = 0.1) {
             // Crear el brazo vertical
             const brazoVertical = new THREE.Mesh(
                 new THREE.CylinderGeometry(grosor, grosor, altura, 32),
@@ -127,42 +153,61 @@ const Stars = ({ title }) => {
             return cruz;
         }
         
-        // Definir posiciones y colores
+        // Definir posiciones y colores de las estrellas
         const estrellasConfig = [
-            { x: -50, y: 10, z: -30, color: 0xff0000 }, // Rojo
-            { x: 50, y: 15, z: -30, color: 0xffff00 }, // Amarillo
-            { x: 5, y: 20, z: -30, color: 0x0000ff }, // Azul
-            { x: 70, y: 0, z: -30, color: 0xff00ff }, // Amarillo
-            { x: 20, y: -5, z: -30, color: 0xff00ff }, // Magenta
-            { x: -66, y: 25, z: -30, color: 0xff8800 }, // Naranja
-            { x: 55, y: -20, z: -30, color: 0xff8800 }, // Naranja
+            { x: -70, y: 10, z: -30, color: 0XFADC00, rotX: 0, rotY: Math.PI / 8, rotZ: Math.PI / 2 },
+            { x: -55, y: 15, z: -30, color: 0xFADC00, rotX: 0, rotY: Math.PI / 10, rotZ: Math.PI / 2 },
+            { x: -35, y: 20, z: -30, color: 0xFADC00, rotX: 0, rotY: Math.PI, rotZ: Math.PI / 2},
+            { x: 35, y: 20, z: -30, color: 0xFADC00, rotX: 0, rotY: - Math.PI / 6, rotZ: Math.PI / 2},
+            { x: 55, y: 15, z: -30, color: 0xFADC00, rotX: 0, rotY: 3 * Math.PI / 4, rotZ: Math.PI / 2 },
+            { x: 70, y: 10, z: -30, color: 0xFADC00, rotX: 0, rotY: 3 * Math.PI / 3.5, rotZ: Math.PI / 2 },
+            /*{ x: 55, y: -20, z: -30, color: 0xff8800 }, // Naranja
             { x: -45, y: -20, z: -30, color: 0xffff00 }, // Magenta
             { x: 30, y: -20, z: -30, color: 0xff0000 }, // Rojo
             { x: -25, y: -5, z: -30, color: 0xff8800 }, // Naranja
             { x: 5, y: 0, z: -30, color: 0xff0000 }, // Rojo
             { x: -5, y: -23, z: -30, color: 0x0000ff }, // Azul
-            { x: -25, y: 28, z: -30, color: 0x0000ff } // Azul
+            { x: -25, y: 28, z: -30, color: 0x0000ff }*/ // Azul
         ];
         
         // Definir posiciones y colores para círculos
         const circulosConfig = [
-            { x: -30, y: 0, z: -20, color: 0xff0000, radio: 0.5 }, // Rojo
-            { x: 30, y: 10, z: -20, color: 0x00ff00, radio: 1 }, // Verde
-            { x: 0, y: -15, z: -20, color: 0x0000ff, radio: 0.5 }, // Azul
-            { x: -55, y: -10, z: -20, color: 0xffff00, radio: 1 }, // Amarillo
-            { x: 60, y: 20, z: -20, color: 0xff00ff, radio: 1 }, // Magenta
-            { x: 40, y: 25, z: -20, color: 0xff00ff, radio: 0.5 }, // Magenta
-            { x: -40, y: 25, z: -20, color: 0xffff00, radio: 1 }, // Amarillo
+            { x: -60, y: 20, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: -55, y: 0, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: -40, y: 25, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: -40, y: -18, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: -20, y: -15, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: -20, y: 15, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: 0, y: -28, z: -20, color: 0x47FCED, radio: 0.3},
+            { x: 10, y: 26, z: -20, color: 0x47FCED, radio: 0.3},
+            { x: 18, y: 20, z: -20, color: 0x47FCED, radio: 0.3},
+            { x: 30, y: -25, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: 40, y: -10, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: 40, y: 10, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: 47, y: 25, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: 60, y: 25, z: -20, color: 0x47FCED, radio: 0.3 },
+            { x: 60, y: -25, z: -20, color: 0x47FCED, radio: 0.3 },
         ];
 
         const crucesConfig = [
-            { x: -10, y: 0, z: -20, color: 0xff0000, altura: 3, ancho: 2, grosor: 0.2 }, // Rojo
-            { x: 10, y: 10, z: -20, color: 0x00ff00, altura: 3, ancho: 2, grosor: 0.2 }, // Verde
-            { x: 0, y: -10, z: -20, color: 0x0000ff, altura: 3, ancho: 2, grosor: 0.2 }, // Azul
-            { x: -20, y: 10, z: -20, color: 0xffff00, altura: 3, ancho: 2, grosor: 0.2 }, // Amarillo
-            { x: 20, y: -20, z: -20, color: 0xff00ff, altura: 3, ancho: 2, grosor: 0.2 }, // Magenta
-            { x: 35, y: 0, z: -20, color: 0xff00ff, altura: 3, ancho: 2, grosor: 0.2 }, // Magenta
-            { x: -50, y: -0, z: -20, color: 0x0000ff, altura: 3, ancho: 2, grosor: 0.2 }, // Azul
+            { x: -55, y: -25, z: -20, color: 0xFF5AAA, altura: 1.5, ancho: 1.5, grosor: 0.1 },
+            { x: -40, y: -10, z: -20, color: 0x81E138, altura: 1.5, ancho: 1.5, grosor: 0.1 },
+            { x: -30, y: 25, z: -20, color: 0x81E138, altura: 1.5, ancho: 1.5, grosor: 0.1 },
+            { x: 20, y: -20, z: -20, color: 0x81E138, altura: 1.5, ancho: 1.5, grosor: 0.1 },
+            { x: 28, y: 10, z: -20, color: 0x81E138, altura: 1.5, ancho: 1.5, grosor: 0.1 },
+            { x: 35, y: 22, z: -20, color: 0xFF5AAA, altura: 1.5, ancho: 1.5, grosor: 0.1 },
+            { x: 50, y: -25, z: -20, color: 0xFF5AAA, altura: 1.5, ancho: 1.5, grosor: 0.1 },
+            { x: 60, y: 0, z: -20, color: 0xFF5AAA, altura: 1.5, ancho: 1.5, grosor: 0.1 },
+        ];
+        
+        // Configuración de líneas con posición y rotación separadas
+        const lineasConfig = [
+            { x: -70, y: 28, z: -30, altura: 30},
+            { x: -55, y: 32, z: -30, altura: 25},
+            { x: -35, y: 32, z: -30, altura: 18},
+            { x: 35, y: 32, z: -30, altura: 18},
+            { x: 55, y: 32, z: -30, altura: 25},
+            { x: 70, y: 28, z: -30, altura: 30},
         ];
 
         const estrellas = [];
@@ -174,14 +219,17 @@ const Stars = ({ title }) => {
             const estrella = crearEstrella(config.color);
             estrella.position.set(config.x, config.y, config.z);
 
-            const linea = crearLineaVertical();
-            linea.position.set(config.x, config.y + 4, config.z);
-            estrella.lookAt(camera.position);
+            estrella.rotation.set(config.rotX, config.rotY, config.rotZ);
         
             scene.add(estrella);
-            scene.add(linea);
-
             estrellas.push(estrella);
+        });
+
+        // Crear y agregar líneas a la escena
+        lineasConfig.forEach((config) => {
+            const linea = crearLineaVertical(config.altura);
+            linea.position.set(config.x, config.y, config.z);
+            scene.add(linea);
         });
 
         // Crear y posicionar círculos con colores fijos
@@ -200,12 +248,224 @@ const Stars = ({ title }) => {
             cruces.push(cruz);
         });
 
+        // ------------------- Galaxia
+
+        const galaxies = [];
+        const galaxyMaterials = [];
+        const galaxyTitles = [
+            "Peligros de Salud Social [R]",
+            "Peligros Digitales [M]",
+            "Peligros de Salud Física [C]",
+            "Peligros de Salud Mental [V]"
+        ];
+        const galaxyUrls = [
+            "/ninos/salud_social",
+            "/ninos/peligros_digitales",
+            "/ninos/salud_fisica",
+            "/ninos/salud_mental"
+        ];
+        const galaxyPositions = [
+            new THREE.Vector3(-13, 1, 0),
+            new THREE.Vector3(13, 1, 0),
+            new THREE.Vector3(0, -7, 0),
+            new THREE.Vector3(0, 10, 0),
+        ];
+
+        const raycaster = new THREE.Raycaster();
+        const mouse = new THREE.Vector2();
+        let isAnimating = false;
+
+        // Crear contenedor de texto central
+        const centralDiv = document.createElement('div');
+        centralDiv.style.position = 'absolute';
+        centralDiv.style.top = '50%';
+        centralDiv.style.left = '50%';
+        centralDiv.style.transform = 'translate(-50%, -50%)';
+        centralDiv.style.color = 'white';
+        centralDiv.style.fontSize = '24px';
+        centralDiv.style.textAlign = 'center';
+        centralDiv.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        centralDiv.style.padding = '20px';
+        centralDiv.style.borderRadius = '10px';
+        centralDiv.style.display = 'flex';
+        centralDiv.style.flexDirection = 'column';
+        centralDiv.style.alignItems = 'center';
+        centralDiv.style.width = '400px';
+        document.body.appendChild(centralDiv);
+
+        const centralText = document.createElement('p');
+        centralText.textContent = "¿A dónde vamos a ir hoy? <> (Seleccione una de las siguientes galaxias)";
+        centralDiv.appendChild(centralText);
+
+        const confirmButton = document.createElement('button');
+        confirmButton.textContent = "Confirmar";
+        confirmButton.style.marginTop = '10px';
+        confirmButton.style.padding = '12px 25px';
+        confirmButton.style.backgroundColor = '#4CAF50'; // Un verde llamativo
+        confirmButton.style.color = 'white';
+        confirmButton.style.border = 'none';
+        confirmButton.style.borderRadius = '8px'; // Bordes más redondeados
+        confirmButton.style.cursor = 'pointer';
+        confirmButton.style.fontSize = '16px';
+        confirmButton.style.fontWeight = 'bold';
+        confirmButton.style.boxShadow = '0px 4px 6px rgba(0, 0, 0, 0.1)'; // Sombra suave
+        confirmButton.style.transition = 'all 0.3s ease'; // Transiciones suaves
+        confirmButton.style.display = 'none';
+
+        // Efecto hover
+        confirmButton.addEventListener('mouseenter', () => {
+            confirmButton.style.backgroundColor = '#45a049'; // Verde más oscuro
+            confirmButton.style.boxShadow = '0px 6px 8px rgba(0, 0, 0, 0.2)'; // Sombra más pronunciada
+            confirmButton.style.transform = 'scale(1.05)'; // Ligeramente más grande
+        });
+
+        confirmButton.addEventListener('mouseleave', () => {
+            confirmButton.style.backgroundColor = '#4CAF50';
+            confirmButton.style.boxShadow = '0px 4px 6px rgba(0, 0, 0, 0.1)';
+            confirmButton.style.transform = 'scale(1)'; // Volver al tamaño original
+        });
+
+        centralDiv.appendChild(confirmButton);
+
+
+        let selectedGalaxyIndex = -1;
+
+        const createGalaxy = (position, baseColor, rotation = { x: 0, y: 0, z: 0 }) => {
+            const particles = 10000;
+            const spiralArms = 7;
+            const radius = 5;
+            const spread = 0.5;
+            const positions = new Float32Array(particles * 3);
+            const colors = new Float32Array(particles * 3);
+            const color = new THREE.Color(baseColor);
+
+            for (let i = 0; i < particles; i++) {
+                const i3 = i * 3;
+                const r = Math.random() * radius;
+                const angle = (i % spiralArms) * (Math.PI * 2 / spiralArms) + r * 0.5 + Math.random() * spread;
+
+                const x = Math.cos(angle) * r + (Math.random() - 0.5) * spread;
+                const y = (Math.random() - 0.5) * spread;
+                const z = Math.sin(angle) * r + (Math.random() - 0.5) * spread;
+
+                positions[i3] = x;
+                positions[i3 + 1] = y;
+                positions[i3 + 2] = z;
+
+                const variation = Math.random() * 0.4 - 0.2;
+                const adjustedColor = color.clone().offsetHSL(0, 0, variation);
+
+                colors[i3] = adjustedColor.r;
+                colors[i3 + 1] = adjustedColor.g;
+                colors[i3 + 2] = adjustedColor.b;
+            }
+
+            const geometry = new THREE.BufferGeometry();
+            geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+            geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+            const material = new THREE.PointsMaterial({
+                size: 0.05,
+                vertexColors: true,
+            });
+
+            const galaxy = new THREE.Points(geometry, material);
+            galaxy.position.copy(position);
+            galaxy.rotation.set(rotation.x, rotation.y, rotation.z);
+
+            scene.add(galaxy);
+            galaxies.push(galaxy);
+            galaxyMaterials.push(material);
+        };
+
+        galaxyPositions.forEach((pos, index) => {
+            const colors = ['#ff0000', '#9d4edd', '#0cb7f2', '#00ff00'];
+            const rotations = [
+                { x: Math.PI / 5, y: 0, z: 0 },
+                { x: Math.PI / 5, y: 0, z: 0 },
+                { x: Math.PI / 17, y: 0, z: 0 },
+                { x: Math.PI / 3, y: 0, z: 0 },
+            ];
+            createGalaxy(pos, colors[index], rotations[index]);
+        });
+
+        camera.position.set(0, 0, 18);
+
+        const moveToGalaxy = (galaxy, title, url) => {
+            if (isAnimating) return;
+            isAnimating = true;
+
+            const duration = 1.5;
+            const distance = 9;
+            const direction = new THREE.Vector3().subVectors(camera.position, galaxy.position).normalize();
+            const targetPosition = new THREE.Vector3().copy(galaxy.position).add(direction.multiplyScalar(distance));
+            const startPosition = new THREE.Vector3().copy(camera.position);
+            let elapsed = 0;
+
+            const animateMove = () => {
+                elapsed += 0.01;
+                const t = Math.min(elapsed / duration, 1);
+
+                camera.position.lerpVectors(startPosition, targetPosition, t);
+                camera.lookAt(galaxy.position);
+
+                if (t < 1) {
+                    requestAnimationFrame(animateMove);
+                } else {
+                    window.location.href = url;
+                }
+            };
+
+            animateMove();
+        };
+
+        const onClick = (event) => {
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+            raycaster.setFromCamera(mouse, camera);
+            const intersects = raycaster.intersectObjects(galaxies);
+
+            galaxyMaterials.forEach((material) => {
+                material.size = 0.05;
+                material.color.setHex(0xffffff);
+            });
+
+            if (intersects.length > 0) {
+                const targetGalaxy = intersects[0].object;
+                const galaxyIndex = galaxies.indexOf(targetGalaxy);
+
+                if (galaxyIndex !== -1) {
+                    selectedGalaxyIndex = galaxyIndex;
+
+                    galaxyMaterials[galaxyIndex].size = 0.1;
+                    galaxyMaterials[galaxyIndex].color.setHex(0xffffff);// 
+
+                    centralText.textContent = `Iremos a ${galaxyTitles[galaxyIndex]}, ¿Correcto?`;
+                    confirmButton.style.display = 'block';
+                }
+            }
+        };
+
+        confirmButton.addEventListener('click', () => {
+            if (selectedGalaxyIndex !== -1) {
+                moveToGalaxy(galaxies[selectedGalaxyIndex], galaxyTitles[selectedGalaxyIndex], galaxyUrls[selectedGalaxyIndex]);
+            }
+        });
+
+        renderer.domElement.addEventListener('click', onClick);
+        
+        // -------------------
         camera.position.z = 20;
 
+        
         // Crear el ciclo de animación
         const animate = () => {
             requestAnimationFrame(animate);
-
+            galaxies.forEach((galaxy, index) => {
+                const speed = 0.0002 + index * 0.0002;//velocidad del giro de animacion
+                galaxy.rotation.y += speed;
+            });
             renderer.render(scene, camera);
         };
     
@@ -218,6 +478,8 @@ const Stars = ({ title }) => {
         return () => {
             renderer.dispose();
             scene.clear();
+            document.body.removeChild(renderer.domElement);
+            centralDiv.remove();
         };
     }, []);
 
