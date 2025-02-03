d81547a45bc6cbd3b2e1e744271b1c285959f97b
Miguel Angel <miguel16hr@gmail.com>
2025-02-03 18:01:43 -0500

    actualizacion de ulimos cambios rama Miguel

--- src/pages/galaxias/SaludSocial.jsx
+++ src/pages/galaxias/SaludSocial.jsx
@@ -3,26 +3,24 @@ import * as THREE from 'three';
 
 export default function EsferaTexturizada() {
     const [currentTextureIndex, setCurrentTextureIndex] = useState(0);
+    const [isZooming, setIsZooming] = useState(false);  // Estado para controlar la animación de acercamiento
 
-    // Lista de texturas disponibles
     const textures = [
         '/assets/2k_makemake_fictional.jpg',
         '/assets/2k_haumea_fictional.jpg',
         '/assets/earthx5400x2700.jpg',
     ];
 
-    // Lista de textos correspondientes a las texturas
     const texts = [
-        "Tipo de riesgo: Peligro digital\nPlaneta: Planeta KIO\nTamaño del planeta: 1.737,4 km (satélite que gira alrededor del mundo dictariano)\nComposición: Tierra árida\nNombre del riesgo: Ciberbullying\nNivel de riesgo: Alto\nAmbiente: Tóxico puede llevar a la muerte\nTemperatura: extremadamente o frío=-30°C es muerte   o caliente hasta 127ºC es muerte\nVillano: Ciberbull",
+        "Tipo de riesgo: Peligro digital\nPlaneta: Planeta KIO\nTamaño del planeta: 1.737,4 km\nComposición: Tierra árida\nNombre del riesgo: Ciberbullying\nNivel de riesgo: Alto\nAmbiente: Tóxico\nTemperatura: -30°C a 127°C\nVillano: Ciberbull",
         "En este planeta....",
         "En este otro planeta....",
     ];
 
-    // Lista de URLs de los planetas
     const planetUrls = [
-        '/ninos/salud_social/planeta_kio',  // URL para el primer planeta
-        'https://www.planetox.com',    // URL para el segundo planeta
-        'https://www.planetoye.com',   // URL para el tercer planeta
+        '/ninos/salud_social/planeta_kio',
+        '/ninos/salud_social/planeta_2',
+        '/ninos/salud_social/planeta_3',
     ];
 
     useEffect(() => {
@@ -31,29 +29,27 @@ export default function EsferaTexturizada() {
         const renderer = new THREE.WebGLRenderer({ antialias: true });
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.setPixelRatio(window.devicePixelRatio);
+        document.body.style.overflow = 'hidden';
+        document.body.style.margin = '0';
+        document.body.style.padding = '0';
         document.body.appendChild(renderer.domElement);
 
-        // Fondo estrellado
         const spaceTexture = new THREE.TextureLoader().load('/assets/2k_stars.jpg');
         scene.background = spaceTexture;
 
-        // Crear esfera texturizada
-        const sphereGeometry = new THREE.SphereGeometry(6, 64, 64); // Esfera con mayor detalle
+        const sphereGeometry = new THREE.SphereGeometry(6, 64, 64);
         const sphereMaterial = new THREE.MeshStandardMaterial({
             map: new THREE.TextureLoader().load(textures[currentTextureIndex]),
         });
         const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        sphere.position.set(0, 0, 0);
         scene.add(sphere);
 
-        // Luz
         const pointLight = new THREE.PointLight(0xffffff, 1, 100);
         pointLight.position.set(2, 5, 10);
         scene.add(pointLight);
 
         camera.position.set(0, 0, 18);
 
-        // Ajustar el tamaño y posición de la esfera al centro de la pantalla
         const resizeHandler = () => {
             camera.aspect = window.innerWidth / window.innerHeight;
             camera.updateProjectionMatrix();
@@ -61,113 +57,136 @@ export default function EsferaTexturizada() {
         };
         window.addEventListener('resize', resizeHandler);
 
-// Contenedor principal para texto y botones
-const mainDiv = document.createElement('div');
-mainDiv.style.position = 'absolute';
-mainDiv.style.top = '0';
-mainDiv.style.left = '0';
-mainDiv.style.width = '100%';
-mainDiv.style.height = '100%';
-mainDiv.style.display = 'flex';
-mainDiv.style.flexDirection = 'column';
-mainDiv.style.alignItems = 'center'; // Centrado horizontal
-mainDiv.style.justifyContent = 'center'; // Centrado vertical
-mainDiv.style.color = 'white';
-mainDiv.style.pointerEvents = 'none'; // Asegura que Three.js funcione correctamente
-document.body.appendChild(mainDiv);
+        //contenedor principal 
+        const mainDiv = document.createElement('div');
+        mainDiv.style.position = 'absolute';
+        mainDiv.style.top = '5%';
+        mainDiv.style.left = '0';
+        mainDiv.style.width = '100%';
+        mainDiv.style.height = '100%';
+        mainDiv.style.display = 'flex';
+        mainDiv.style.flexDirection = 'column';
+        mainDiv.style.alignItems = 'center';
+        mainDiv.style.justifyContent = 'center';
+        mainDiv.style.color = 'white';
+        mainDiv.style.pointerEvents = 'none';
+        document.body.appendChild(mainDiv);
 
-// Título centrado
-const title = document.createElement('h1');
-title.textContent = 'Bienvenidos a la sección de Salud Social';
-title.style.fontSize = '32px';
-title.style.textAlign = 'center'; // Asegura que el texto esté centrado
-title.style.pointerEvents = 'auto';
-mainDiv.appendChild(title);
+        //texto principal
+        const title = document.createElement('h1');
+        title.textContent = 'Bienvenidos a la sección de Salud Social';
+        title.style.fontSize = '40px';
+        title.style.textAlign = 'center';
+        title.style.pointerEvents = 'auto';
+        title.style.position = 'absolute';
+        title.style.left = '50%';
+        title.style.top = '5%';
+        title.style.transform = 'translate(-50%, -50%)';
+        mainDiv.appendChild(title);
 
-        // Contenedor de texto
+        //contenedor texto
         const textContainer = document.createElement('div');
-        textContainer.style.margin = 'auto';
-        textContainer.style.backgroundColor = 'rgba(255, 153, 135, 0.27)';
+        mainDiv.style.justifyContent = '';
+        mainDiv.style.paddingTop = '450px';
+
+        textContainer.style.fontSize = '20px';
+        textContainer.style.backgroundColor = 'rgba(252, 107, 102, 0.32)';
         textContainer.style.padding = '24px';
+        textContainer.style.margin = '32px';
         textContainer.style.borderRadius = '10px';
         textContainer.style.pointerEvents = 'auto';
-        textContainer.innerHTML = texts[currentTextureIndex].replace(/\n/g, '<br />'); // Reemplaza saltos de línea
+        textContainer.innerHTML = texts[currentTextureIndex].replace(/\n/g, '<br />');
 
-        // Controles (botones en forma de flechas)
+        //contenedor de los botones <>
+
         const controlsDiv = document.createElement('div');
         controlsDiv.style.display = 'flex';
-        controlsDiv.style.justifyContent = 'space-between';
-        controlsDiv.style.width = '100%';
+        controlsDiv.style.gap = '64px';
         controlsDiv.style.pointerEvents = 'auto';
 
-        // Botón izquierdo (anterior)
-        const leftArrow = document.createElement('button');
-        leftArrow.innerHTML = '⟵';
-        leftArrow.style.fontSize = '32px';
-        leftArrow.style.background = 'rgba(255, 255, 255, 0.2)'; // Fondo transparente pero visible
-        leftArrow.style.border = 'none';
-        leftArrow.style.color = 'white';
-        leftArrow.style.cursor = 'pointer';
-        leftArrow.style.margin = '20px';
-        leftArrow.style.padding = '10px';
-        leftArrow.style.borderRadius = '10px';
-        controlsDiv.appendChild(leftArrow);
+        const createButton = (text, onClick) => {
+            const button = document.createElement('button');
+            button.innerHTML = text;
+            button.style.fontSize = '24px';
+            button.style.background = 'linear-gradient(135deg,rgb(207, 47, 47),rgb(114, 35, 35))';
+            button.style.border = 'none';
+            button.style.color = 'white';
+            button.style.cursor = 'pointer';
+            button.style.padding = '12px 20px';
+            button.style.borderRadius = '20px';
+            button.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+            button.style.transition = 'all 0.3s ease';
+            button.addEventListener('mouseover', () => {
+                button.style.transform = 'scale(1.05)';
+                button.style.boxShadow = '0 6px 8px rgba(0, 0, 0, 0.15)';
+            });
+            button.addEventListener('mouseout', () => {
+                button.style.transform = 'scale(1)';
+                button.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+            });
+            button.addEventListener('click', onClick);
+            return button;
+        };
 
-        // Botón para abrir URL
-        const urlButton = document.createElement('button');
-        urlButton.innerHTML = 'Ver más';
-        urlButton.style.fontSize = '24px';
-        urlButton.style.background = 'rgba(255, 255, 255, 0.5)';
-        urlButton.style.border = 'none';
-        urlButton.style.color = 'black';
-        urlButton.style.cursor = 'pointer';
-        urlButton.style.margin = '20px';
-        urlButton.style.padding = '10px';
-        urlButton.style.borderRadius = '10px';
-        urlButton.addEventListener('click', () => {
-            window.open(planetUrls[currentTextureIndex], '_blank'); // Redirige a la URL del planeta actual
+        const changeTexture = (direction) => {
+            setCurrentTextureIndex((prevIndex) => {
+                let nextIndex = prevIndex;
+                if (direction === 'next') {
+                    nextIndex = (prevIndex + 1) % textures.length;
+                } else if (direction === 'prev') {
+                    nextIndex = (prevIndex - 1 + textures.length) % textures.length;
+                }
+
+                // Cargar nueva textura antes de actualizar el estado
+                const newTexture = new THREE.TextureLoader().load(textures[nextIndex]);
+                sphere.material.map = newTexture;
+                sphere.material.needsUpdate = true;
+
+                // Actualizar el texto del contenedor
+                textContainer.innerHTML = texts[nextIndex].replace(/\n/g, '<br />');
+
+                return nextIndex; // Retorna el nuevo índice para actualizar el estado correctamente
+            });
+        };
+
+        const backButton = createButton('← Regresar', () => {
+            window.history.back();
         });
-        controlsDiv.appendChild(urlButton);
+        
+        backButton.style.position = 'absolute';
+        backButton.style.left = '20px';
+        backButton.style.top = '20px'; 
+        backButton.style.zIndex = '1000'; // para que el boton este por encima de todo
+        
+        document.body.appendChild(backButton);
+        
+        controlsDiv.appendChild(createButton('⟵', () => changeTexture('prev')));
+        controlsDiv.appendChild(createButton('Ver más', () => {
+            // Inicia la animación de acercamiento
+            setIsZooming(true);
 
-        // Botón derecho (siguiente)
-        const rightArrow = document.createElement('button');
-        rightArrow.innerHTML = '⟶';
-        rightArrow.style.fontSize = '32px';
-        rightArrow.style.background = 'rgba(255, 255, 255, 0.2)'; // Fondo transparente pero visible
-        rightArrow.style.border = 'none';
-        rightArrow.style.color = 'white';
-        rightArrow.style.cursor = 'pointer';
-        rightArrow.style.margin = '20px';
-        rightArrow.style.padding = '10px';
-        rightArrow.style.borderRadius = '10px';
-        controlsDiv.appendChild(rightArrow);
-
-        // Añadir contenedor de texto y controles al final
+            // Llamar a la función de animación de acercamiento
+            setTimeout(() => {
+                setIsZooming(false);  // Detener la animación después de 1 segundo
+                window.location.href = planetUrls[currentTextureIndex];  // Redirige a la URL en la misma ventana
+            }, 1000);  // Espera 1 segundo antes de redirigir
+        }));
+        controlsDiv.appendChild(createButton('⟶', () => changeTexture('next')));
+        
         mainDiv.appendChild(textContainer);
         mainDiv.appendChild(controlsDiv);
 
-        // Función para cambiar textura y texto
-        const changeTexture = (direction) => {
-            let nextIndex = currentTextureIndex;
-            if (direction === 'next') {
-                nextIndex = (currentTextureIndex + 1) % textures.length;
-            } else if (direction === 'prev') {
-                nextIndex = (currentTextureIndex - 1 + textures.length) % textures.length;
+        // Función para manejar la animación de acercamiento de la cámara
+        const zoomIn = () => {
+            if (isZooming && camera.position.z > 7) {
+                camera.position.z -= 0.07;  // Ajusta la velocidad de acercamiento
             }
-            const newTexture = new THREE.TextureLoader().load(textures[nextIndex]);
-            sphere.material.map = newTexture;
-            sphere.material.needsUpdate = true;
-            setCurrentTextureIndex(nextIndex); // Actualiza el índice actual
-            textContainer.innerHTML = texts[nextIndex].replace(/\n/g, '<br />'); // Actualiza el texto con saltos de línea
         };
 
-        rightArrow.addEventListener('click', () => changeTexture('next'));
-        leftArrow.addEventListener('click', () => changeTexture('prev'));
-
-        // Animación
         const animate = () => {
             requestAnimationFrame(animate);
-            sphere.rotation.y += 0.0005; // Rotación animada
+            zoomIn();  // Llama a la función de acercamiento en cada frame
+            sphere.rotation.y += 0.0005;
             renderer.render(scene, camera);
         };
         animate();
@@ -178,7 +197,7 @@ mainDiv.appendChild(title);
             document.body.removeChild(renderer.domElement);
             document.body.removeChild(mainDiv);
         };
-    }, [currentTextureIndex, textures, texts, planetUrls]);
+    }, [currentTextureIndex, textures, texts, planetUrls, isZooming]);
 
     return null;
 }
